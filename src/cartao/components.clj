7489a2e1dcde93bdef1024b59329f08a34b07e46
Lucas Bernardo <lbernardo.brito@gmail.com>
2021-05-20 14:31:11 -0300

    date by calendar

--- src/cartao/components.clj
+++ src/cartao/components.clj
@@ -1,10 +1,15 @@
 (ns cartao.components
-  (:import (java.text SimpleDateFormat)))
+  (:import (java.text SimpleDateFormat))
+  (:import (java.util GregorianCalendar)))
 
 
 (defn format-date! [date]
   (let [simple-date-format (SimpleDateFormat. "dd/MM/yyyy")]
     (.format simple-date-format date)))
+
+
+(defn date-by-calendar [day month year]
+  (.getTime (GregorianCalendar. year (dec month) day)))
 
 
 (defn clear-data
--- src/cartao/controller.clj
+++ src/cartao/controller.clj
@@ -2,7 +2,8 @@
   (:require [cartao.app.model :as c.model]
             [cartao.ports.secondary.repositories.account :as c.r.account]
             [cartao.ports.secondary.repositories.creditcard :as c.r.creditcard]
-            [cartao.ports.secondary.repositories.purchase :as c.r.purchase])
+            [cartao.ports.secondary.repositories.purchase :as c.r.purchase]
+            [cartao.components :as c.components])
   (:import (java.util Calendar)))
 
 
@@ -13,12 +14,12 @@
   (c.model/create-model-creditcard "5274 6223 1271 9635" "620" "01/23" 3000))
 
 (defn create-purchases-default []
-  [(c.model/create-model-purchase (.getTime (java.util.GregorianCalendar. 2021 Calendar/JANUARY 1)) "BK" "Food" :food 40M)
-   (c.model/create-model-purchase (.getTime (java.util.GregorianCalendar. 2021 Calendar/JANUARY 2)) "MD" "Food" :food 200M)
-   (c.model/create-model-purchase (.getTime (java.util.GregorianCalendar. 2021 Calendar/JANUARY 2)) "BIG" "Merchant" :merchant 800M)
-   (c.model/create-model-purchase (.getTime (java.util.GregorianCalendar. 2021 Calendar/FEBRUARY 3)) "FRAG" "Merchant" :merchant 50M)
-   (c.model/create-model-purchase (.getTime (java.util.GregorianCalendar. 2021 Calendar/FEBRUARY 3)) "Iguatemi" "Mall" :mall 823M)
-   (c.model/create-model-purchase (.getTime (java.util.GregorianCalendar. 2021 Calendar/MARCH 3)) "Iguatemi" "Mall" :mall 2500M)
+  [(c.model/create-model-purchase (c.components/date-by-calendar 1 1 2021) "BK" "Food" :food 40M)
+   (c.model/create-model-purchase (c.components/date-by-calendar 2 1 2021) "MD" "Food" :food 200M)
+   (c.model/create-model-purchase (c.components/date-by-calendar 2 1 2021) "BIG" "Merchant" :merchant 800M)
+   (c.model/create-model-purchase (c.components/date-by-calendar 3 2 2021) "FRAG" "Merchant" :merchant 50M)
+   (c.model/create-model-purchase (c.components/date-by-calendar 3 2 2021) "Iguatemi" "Mall" :mall 823M)
+   (c.model/create-model-purchase (c.components/date-by-calendar 3 3 2021)  "Iguatemi" "Mall" :mall 2500M)
    ])
 
 (defn config [conn]
