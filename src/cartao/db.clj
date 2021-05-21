3a03598b0e4563332ca6c7454eec322cfdb08b74
Lucas Bernardo <lbernardo.brito@gmail.com>
2021-05-21 13:17:37 -0300

    fix cardinality

--- src/cartao/db.clj
+++ src/cartao/db.clj
@@ -41,28 +41,28 @@
 
              {:db/ident       :purchase/date
               :db/valueType   :db.type/instant
-              :db/cardinality :db.cardinality/many
+              :db/cardinality :db.cardinality/one
               :db/doc         "Purchase date"
               }
              {:db/ident       :purchase/establishment
               :db/valueType   :db.type/string
-              :db/cardinality :db.cardinality/many
+              :db/cardinality :db.cardinality/one
               :db/doc         "Purchase establishment"
               }
              {:db/ident       :purchase/category
               :db/valueType   :db.type/string
-              :db/cardinality :db.cardinality/many
+              :db/cardinality :db.cardinality/one
               :db/doc         "Purchase category"
               }
              {
               :db/ident       :purchase/category-id
               :db/valueType   :db.type/keyword
-              :db/cardinality :db.cardinality/many
+              :db/cardinality :db.cardinality/one
               :db/doc         "Category ID"
               }
              {:db/ident       :purchase/amount
               :db/valueType   :db.type/bigdec
-              :db/cardinality :db.cardinality/many
+              :db/cardinality :db.cardinality/one
               :db/doc         "Purchase amount"
               }
              ]
